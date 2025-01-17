e520c7b702efbe3bc3c3ad63b1cc46a7346494b9
bobirrahmatov <55207867+bobirrahmatov@users.noreply.github.com>
2025-01-17 07:11:06 +0100

    projects added

--- src/data/clients.config.tsx
+++ src/data/clients.config.tsx
@@ -32,6 +32,35 @@ export const CLIENTWORKS = [
       "https://firebasestorage.googleapis.com/v0/b/portfolio-e69c5.firebasestorage.app/o/Safe_Gm3.mp4?alt=media&token=4dc1c14e-367f-49a8-83b5-6e10eeacb43f",
   },
   {
+    title: "inVerus",
+    dates: "20 Dec 2024 - 2nd Jan 2024",
+    worktype: "Website, Platform",
+    testimonial:
+      "",
+    featured: true,
+    description:
+      "inVerus is a platform dedicated to the next evolution in identity fraud prevention, leveraging AI-driven solutions to combat deep fakes and escalating attacks, protecting businesses from the $43 billion annual threat of identity fraud.",
+    features:
+      "",
+    technologies: [
+      "Next.js",
+      "Typescript",
+      "CMS",
+      "Framer Motion",
+      "TailwindCSS",
+      "Shadcn UI",
+    ],
+    links: [
+      {
+        type: "Website",
+        href: "https://inverus.vercel.app/",
+        icon: <Icons.globe className="size-3" />,
+      },
+    ],
+    image: "https://firebasestorage.googleapis.com/v0/b/portfolio-e69c5.firebasestorage.app/o/undefined.png?alt=media&token=ce6c7abc-84ed-47b5-8df9-19baad918be9",
+    video: ""
+  },
+  {
     title: "Web3 NFT-ETH Exchange dApp",
     dates: "10th Oct 2024 - 17th Oct 2024",
     worktype: "dApp - Web3",
